d097d1c78230f5ab45f4d2bccacfd4d678011916
galaxyan12 <jody@student.untan.ac.id>
2021-11-02 12:58:18 +0700

    init

--- app/src/main/java/com/kalbarprov/laporbencana/LoginActivity.java
+++ app/src/main/java/com/kalbarprov/laporbencana/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,42 +34,38 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
         progressBar = findViewById(R.id.progressBarLogin);
-        btnLogin = findViewById(R.id.button_Login);
+        btnLogin = findViewById(R.id.buttonLogin);
         btnLogin.setOnClickListener(v -> {
-
+            progressBar.setVisibility(View.VISIBLE);
             if(TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())){
-                Toast.makeText(LoginActivity.this, "Masukkan Username & Password", Toast.LENGTH_LONG).show();
+                Toast.makeText(LoginActivity.this, "Masukkan Username & Password", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
             } else {
-                //Login
-                login();
+                doLogin();
+                progressBar.setVisibility(View.GONE);
             }
-
         });
     }
 
-    public void login(){
+    private void doLogin() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername(username.getText().toString());
         loginRequest.setPassword(password.getText().toString());
-        Call<LoginResponse> loginResponseCall = RetrofitClient.getLoginInterface().userLogin(username.getText().toString(), password.getText().toString());
+        Call<LoginResponse> loginResponseCall = RetrofitClient.getLoginInterface().userLogin(loginRequest.getUsername(), loginRequest.getPassword());
         loginResponseCall.enqueue(new Callback<LoginResponse>() {
             @Override
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 if(response.isSuccessful()){
-                    progressBar.setVisibility(View.VISIBLE);
-                    Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_LONG).show();
-                    LoginResponse loginResponse = response.body();
-
+                    Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
                     new Handler().postDelayed(() -> {
-                        progressBar.setVisibility(View.GONE);
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class).putExtra("data", loginResponse.getEmail()));
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     }, 700);
                 }
             }
 
             @Override
             public void onFailure(Call<LoginResponse> call, Throwable t) {
-                Toast.makeText(LoginActivity.this, "Failed " +t.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "Failure " +t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
